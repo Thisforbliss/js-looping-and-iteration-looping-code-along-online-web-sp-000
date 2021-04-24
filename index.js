fcf8fea35c28e85789c1957bbc6cb94c4122c180
Malcolm Leake <echelon1089@gmail.com>
2021-04-24 21:24:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-
-const friends = ["Ada", "Brendan", "Ali"];
-
-function writeCards(friends) {
-  for (let i = 0; i < friends.length; i++)
-  {console.log('Thank you, ${friends[i]}, for the wonderful birthday gift!');}
-  return friends;
+const gifts = ["teddy bear", "drone", "doll"];
+ 
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+ 
+  return gifts;
 }
+ 
+wrapGifts(gifts);
+
+function wrapGifts(gifts) {
+    let i = 0; // the initialization moved OUTSIDE the body of the loop!
+    while (i < gifts.length) {
+      console.log(`Wrapped ${gifts[i]} and added a bow!`);
+      i++; // the iteration moves INSIDE the body of the loop!
+    }
+   
+    return gifts;
+  }
+   
+  wrapGifts(gifts);
